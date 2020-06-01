6dc527e0250a745a48a796003246fbd3b107a3fb
wangyizi <405572855@qq.com>
2020-06-01 16:17:27 +0800

    #000

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-27 20:44:32
- * @LastEditTime: 2020-05-28 10:47:22
+ * @LastEditTime: 2020-06-01 16:12:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \government-service-assessment\src\router\index.js
@@ -11,7 +11,8 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [{
+const routes = [
+  {
     path: '/',
     name: 'Dashboard',
     component: () => import( /* webpackChunkName: "dashboard" */ '@/views/dashboard/index.vue')
@@ -21,6 +22,12 @@ const routes = [{
     path: '/citydetail/:cityname',
     name: 'Citydetail',
     component: () => import( /* webpackChunkName: "dashboard" */ '@/views/dashboard/citydetail.vue')
+  },
+  // 服务事项覆盖度
+  {
+    path: '/serviceCoverage',
+    name: 'ServiSceCoverage',
+    component: () => import( /* webpackChunkName: "dashboard" */ '@/views/service-coverage/index.vue')
   },
   // 总体排名
   {
---
+++ src/views/service-coverage/index.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-06-01 10:22:23
+ * @LastEditTime: 2020-06-01 16:14:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \haochapin\src\views\service-coverage\index.vue
+--> 
+<template>
+<div class="serviceCoverage">
+    <v-dashBoardHeader>市级政府网上政务服务能力调查评估</v-dashBoardHeader>
+    <div></div>
+</div>
+</template>
+<script>
+import vDashBoardHeader from '@/components/DashBoardHeader'
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    vDashBoardHeader
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+// .serviceCoverage{}
+</style>
